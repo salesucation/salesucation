2b7eb495e69aad1ced7a0f501acfe76813a4acd2
Richard Hildred <rhildred@gmail.com>
2024-12-20 20:30:06 +0000

    works with ansible_runner

--- stage2.py
+++ stage2.py
@@ -1,31 +1,3 @@
-#!/usr/bin/env python
-
-import os
-import sys
-from collections import namedtuple
-
-from ansible.parsing.dataloader import DataLoader
-from ansible.vars.manager import VariableManager
-from ansible.inventory.manager import Inventory
-from ansible.executor.playbook_executor import PlaybookExecutor
-
-loader = DataLoader()
-
-inventory = Inventory(loader=loader, sources='/home/slotlocker/hosts2')
-variable_manager = VariableManager(loader=loader, inventory=inventory)
-playbook_path = '/home/slotlocker/ls.yml'
-
-if not os.path.exists(playbook_path):
-    print '[INFO] The playbook does not exist'
-    sys.exit()
-
-Options = namedtuple('Options', ['listtags', 'listtasks', 'listhosts', 'syntax', 'connection','module_path', 'forks', 'remote_user', 'private_key_file', 'ssh_common_args', 'ssh_extra_args', 'sftp_extra_args', 'scp_extra_args', 'become', 'become_method', 'become_user', 'verbosity', 'check','diff'])
-options = Options(listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None, forks=100, remote_user='slotlocker', private_key_file=None, ssh_common_args=None, ssh_extra_args=None, sftp_extra_args=None, scp_extra_args=None, become=True, become_method='sudo', become_user='root', verbosity=None, check=False, diff=False)
-
-variable_manager.extra_vars = {'hosts': 'mywebserver'} # This can accomodate various other command line arguments.`
-
-passwords = {}
-
-pbex = PlaybookExecutor(playbooks=[playbook_path], inventory=inventory, variable_manager=variable_manager, loader=loader, options=options, passwords=passwords)
-
-results = pbex.run()
+import ansible_runner
+r = ansible_runner.run(private_data_dir='.', playbook='stage2a.yml')
+print("{}: {}".format(r.status, r.rc))
