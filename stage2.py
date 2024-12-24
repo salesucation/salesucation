f717c39f8f0a4d23f6a6134832c0e0cfc060d6ab
Rich Hildred <rhildred@gmail.com>
2024-12-24 03:18:38 +0000

    install k3d in codespace

--- stage2.py
+++ stage2.py
@@ -1,9 +1,7 @@
 import ansible_runner
 import requests
- 
-url = 'https://raw.githubusercontent.com/salesucation/k3p/rich-sprint2/stage2a.yml'  # Replace with your URL
-local_filename = '/tmp/install/stage2a.yml'  # Replace with your desired local file name
- 
+import shutil
+  
 def download_file(url, local_filename):
     with requests.get(url, stream=True) as r:
         r.raise_for_status()
@@ -12,6 +10,11 @@ def download_file(url, local_filename):
                 f.write(chunk)
     return local_filename
  
-download_file(url, local_filename)
-r = ansible_runner.run(private_data_dir='.', playbook='stage2a.yml')
-print("{}: {}".format(r.status, r.rc))
+if shutil.which("docker"):
+    download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint2/k3d.yml', '/tmp/install/k3d.yml')
+    r = ansible_runner.run(private_data_dir='.', playbook='k3d.yml')
+    print("{}: {}".format(r.status, r.rc))
+else:
+    download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint2/k3s.yml', '/tmp/install/k3s.yml')
+    r = ansible_runner.run(private_data_dir='.', playbook='k3s.yml')
+    print("{}: {}".format(r.status, r.rc))    
