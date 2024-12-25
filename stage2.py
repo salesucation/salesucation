15d93030650af62b8c1a1d8bce9040f864258b37
Rich Hildred <rhildred@gmail.com>
2024-12-25 15:59:52 -0500

    done till knative

--- stage2.py
+++ stage2.py
@@ -9,7 +9,8 @@ def download_file(url, local_filename):
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
     return local_filename
- 
+
+# install a kubernetes 
 if shutil.which("docker"):
     download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint2/k3d.yml', '/tmp/install/k3d.yml')
     r = ansible_runner.run(private_data_dir='.', playbook='k3d.yml')
@@ -17,4 +18,9 @@ if shutil.which("docker"):
 else:
     download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint2/k3s.yml', '/tmp/install/k3s.yml')
     r = ansible_runner.run(private_data_dir='.', playbook='k3s.yml')
-    print("{}: {}".format(r.status, r.rc))    
+    print("{}: {}".format(r.status, r.rc))
+
+# install knative
+download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint2/knative.yml', '/tmp/install/knative.yml')
+r = ansible_runner.run(private_data_dir='.', playbook='knative.yml')
+print("{}: {}".format(r.status, r.rc))
