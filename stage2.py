fa43ce7d5ce2c182aea98c8acb38e96048ccccad
Rich Hildred <rhildred@gmail.com>
2024-12-28 23:46:01 -0500

    only install a kubernetes if there isn't kubectl

--- stage2.py
+++ stage2.py
@@ -11,18 +11,22 @@ def download_file(url, local_filename):
                 f.write(chunk)
     return local_filename
 
-# install a kubernetes 
-if shutil.which("systemd"):
-    download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint3/k3s.yml', '/tmp/install/k3s.yml')
-    r = ansible_runner.run(private_data_dir='.', playbook='k3s.yml')
-    print("{}: {}".format(r.status, r.rc))
-    os.environ["KUBECONFIG"] = "/etc/rancher/k3s/k3s.yaml"
-else:
-    download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint3/k3d.yml', '/tmp/install/k3d.yml')
-    r = ansible_runner.run(private_data_dir='.', playbook='k3d.yml')
-    print("{}: {}".format(r.status, r.rc))
+# install a kubernetes
+if not shutil.which("kubectl"):
+    if shutil.which("systemd"):
+        download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint3/k3s.yml', '/tmp/install/k3s.yml')
+        r = ansible_runner.run(private_data_dir='.', playbook='k3s.yml')
+        print("{}: {}".format(r.status, r.rc))
+        os.environ["KUBECONFIG"] = "/etc/rancher/k3s/k3s.yaml"
+    else:
+        download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint3/k3d.yml', '/tmp/install/k3d.yml')
+        r = ansible_runner.run(private_data_dir='.', playbook='k3d.yml')
+        print("{}: {}".format(r.status, r.rc))
 
 # install knative
 download_file('https://raw.githubusercontent.com/salesucation/k3p/rich-sprint3/knative.yml', '/tmp/install/knative.yml')
 r = ansible_runner.run(private_data_dir='.', playbook='knative.yml')
 print("{}: {}".format(r.status, r.rc))
+
+# make patches
+
