511dafadf6067dd4b3b0eeb413d1d012574f8649
Rich Hildred <rhildred@gmail.com>
2025-02-01 16:48:22 -0500

    make test pass and so that it can be debugged

--- tests.py
+++ tests.py
@@ -5,6 +5,9 @@ http = urllib3.PoolManager()
 
 class testInput(unittest.TestCase):
     def test_func(self):
-        response = http.request('GET', 'http://localhost', headers={
-            'Host': "hello.test.example.com"})
-        self.assertEqual(response, 1)
+        response = http.request('GET', 'http://hello.test.k3p.ca')
+        self.assertEqual(response.data, b'Hello from Salesucation!\n')
+
+
+if __name__ == '__main__':
+  unittest.main()        
