c9a9b3be854b92d06ee07cde54cbb30a153badc1
salesucation <rhildred@salesucation.ca>
2025-02-02 17:09:14 +0000

    run k3d if in a docker container

--- tests.py
+++ tests.py
@@ -1,14 +1,13 @@
 import unittest
 import urllib3
-import os
 
 http = urllib3.PoolManager()
 
 class testInput(unittest.TestCase):
     def test_func(self):
-        #TODO: set host header to match provided CNAME
-        response = http.request('GET', 'http://localhost', headers={
-        "Host": "hello.test.k3p.ca"})
+        #TODO: set host header to match provided CNAME and set http request to EXTERNAL-IP from `kubectl --namespace kourier-system get service kourier`
+        response = http.request('GET', 'http://172.18.0.2', headers={
+        "Host": "hello.test.k3p.dev"})
         self.assertEqual(response.data, b'Hello from Salesucation!\n')
 
 
